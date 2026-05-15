db7f8229c32a612b0f416edfc9ca4c5aa4f05f5b
ErGovindthakur <ergovindthakur@gmail.com>
2026-05-15 11:33:38 +0530

    explore how to setup typescript in project and configure tsconfig.json file

---
+++ 06_TypeScriptProjectSetup/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let greet = (name) => {
+    return `Hello,  ${name}`;
+};
+const name = "Govind Thakur";
+let greet1 = greet(name);
+console.log(greet1);
+//# sourceMappingURL=index.js.map
---
+++ 06_TypeScriptProjectSetup/src/index.ts
@@ -0,0 +1,6 @@
+let greet = (name:string):string => {
+     return `Hello,  ${name}`;
+}
+const name:string = "Govind Thakur";
+let greet1 = greet(name);
+console.log(greet1);
