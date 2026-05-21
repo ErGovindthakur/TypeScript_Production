4bcb9f498e82751834f5b5bcfb20f3b144850155
ErGovindthakur <ergovindthakur@gmail.com>
2026-05-21 23:27:11 +0530

    explore tupples in ts

---
+++ 05_typeAndInterface/src/tupplesTs.ts
@@ -0,0 +1,10 @@
+let chaiTuple : [string,number];
+chaiTuple = ["Masala",20];
+// chaiTuple = [20,"Masala"] // order matters
+
+let userInfo : [string, number, boolean?];
+userInfo = ["Govind",100,true];
+
+const location : readonly[number,number] = [12,12];
+
+const chaiItems: [name:string, price:number] = ["Masala",12]
