cf4f53d0b809775d9a31283722afa047a9448810
ErGovindthakur <ergovindthakur@gmail.com>
2026-05-23 17:35:40 +0530

    explore all wh aspects of using ts

---
+++ 06_PracticeBasicTs/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let myName = "Govind";
+// myName = 123; It will produce an error, of type number is not assignable to type string
+console.log(myName);
+//# sourceMappingURL=index.js.map
